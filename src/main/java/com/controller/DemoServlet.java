76381cbd152af737e33d8ba8b88f5c8cd7fdbf4e
vishwak28 <vishu.khuprao@gmail.com>
2018-11-16 14:44:18 +0530

    commit

---
+++ src/main/java/com/controller/DemoServlet.java
@@ -0,0 +1,36 @@
+package com.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class DemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public DemoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	
+		PrintWriter pw = response.getWriter();
+		String str = request.getParameter("name");
+		pw.println("<h1>Hello"+str+"</h1>");
+
+	
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+
+<form action="DemoServlet">
+<input type = "text" name = "name">
+<input type = "submit" name = "Submit">
+</form>
+
+</body>
+</html>
